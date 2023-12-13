72387177f0594dd356158064af853a76fb0b8706
sec <sec@spsmb.cz>
2023-12-13 09:32:04 +0100

    added descriptions

--- readme.md
+++ readme.md
@@ -1,3 +1,18 @@
-#Lekce 15 - Opakování Java Dates
+# Lekce 15 - Opakování Java Dates
 
-Vrámci opakování vypracujte následující příklady. Výsledky si můžete ověřit v testovací třídě *DateTimeUtilsTest*
+Vrámci opakování práce s datumy vypracujte následující příklady. Výsledky si můžete ověřit v testovací třídě *DateTimeUtilsTest*.
+
+## Zadání
+ 1) *(1 bod)* Vypište aktuální datum ve formátu den.měsíc.rok hodina:minuta (např. 12.12.2023 14:41)
+ 2) *(1 bod)* Naparsujte datum "03.11.2023" pomocí parseru se zadaným fromátem. Správný zápis formátu můžete vyhledat.
+ 3) *(1 bod)* Přičtěte k zadanému datu (LocalDate) 12 hodin a vypište ve formátu 12.12.2023 12:00
+ 4) *(1 bod)* Vypište aktuální den v týdnu v české lokalizaci.
+    * :bulb: Pro dneštní datum bude výsledek: "Dnes je středa."
+ 5) *(2 body)* Definujte proměnou Vánoce (LocalDate), která bude ukazovat na datum 24.12.2023 a vypište kolik dní zbývá do začátku Vánoc pomocí String format();
+
+    * pro výpočet použijte proměnné Vánoce a aktuální datum.
+    * Výsledný výstup z metody bude String ve formátu "Do Vánoc zbývá XY dní."
+
+ 6) *(2 body)* Napište metodu, která porovná dva vložené datumy a vrátí vždy nižší z datumů na první pozici a vyšší na druhé. Pokud je tedy druhé datum nižší než to první, pak oba datumy prohodí prohodí a vrátí je jako list.
+ 7) *(2 body)* Najděte a vypište první pondělí po Vánocích 24.12.2023
+
--- src/main/java/cz/secda1/spsmb/javaDates/DateTimeUtils.java
+++ src/main/java/cz/secda1/spsmb/javaDates/DateTimeUtils.java
@@ -3,21 +3,16 @@ package cz.secda1.spsmb.javaDates;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
-
 public class DateTimeUtils {
 
     /**
      * Vypište aktuální datum ve formátu den.měsíc.rok hodina:minuta (např. 12.12.2023 14:41)
      *
-     * @param date
-     * @return
+     * @param date Libovolné datum a čas (LocalDateTime), které chceme naformátovat.
+     * @return String s naformátovaným datem
      */
     public static String formattedDate(LocalDateTime date) {
         return null;
@@ -26,7 +21,7 @@ public class DateTimeUtils {
     /**
      * Naparsujte datum "03.11.2023" pomocí parseru se zadaným fromátem. Správný zápis formátu můžete vyhledat.
      *
-     * @param dateString
+     * @param dateString String ze kterého vyrobíme datum (LocalDate)
      * @return
      */
     public static LocalDate parseDate(String dateString) {
@@ -46,6 +41,7 @@ public class DateTimeUtils {
     /**
      * Vypište aktuální den v týdnu v české lokalizaci.
      *
+     * @example "Dnes je středa."
      * @return String "Dnes je středa."
      */
     public static String whatsTheDateToday(LocalDate today) {
@@ -54,36 +50,35 @@ public class DateTimeUtils {
 
         String dayNameInCzech = dayOfWeek.getDisplayName(TextStyle.FULL, Locale.getDefault());
 
-        //Sestavte message "Dnes je <denVTýdnu>." pomocí String.format()
+        //Sestavte message "Dnes je <dayNameInCzech>." pomocí String.format()
         String formattedMessage = null;
         return formattedMessage;
     }
 
     /**
      * Definujte proměnou Vánoce (LocalDate), která bude ukazovat na datum 24.12.2023
-     * <p>
-     * 1) Vypište kolik dní zbývá  do začátku Vánoc pomocí String format();
-     * 2) pro výpočet použijte proměnnou Vánoce a aktuální datum.
-     * 3) Výsledný výstup z metody bude String ve formátu "Do Vánoc zbývá XY dní."
+     * a vypište kolik dní zbývá do začátku Vánoc pomocí String format();
      *
-     * @return
+     * 1) pro výpočet použijte proměnné Vánoce a aktuální datum.
+     * 2) Výsledný výstup z metody bude String ve formátu "Do Vánoc zbývá XY dní."
+     *
+     * @return String message
      */
     public static String daysToChristmas() {
         return null;
     }
 
     /**
-     *
-     * @param start
-     * @param end
-     * @return
-     *
      * Napište metodu, která porovná dva vložené datumy a vrátí vždy nižší datum na provní pozici a vyšší na druhé.
      * Pokud je tedy druhé datum nižší než to první, pak oba datumy prohodí prohodí a vrátí je jako list.
      *
      * @examples
      * např. pro start = 01.01.2023 a end = 31.12.2023 vrátí list ve stejném pořadí
      * např. pro start = 31.12.2023  a end = 01.01.2023 vrátí list ve opět s nižším datem na první pozici a s vyšším na pozici druhé, tedy opět 01.01.2023, 31.12.2023.
+     *
+     * @param start první datum pro porovnání
+     * @param end druhé datum pro porovnání
+     * @return List<LocalDate> s oběma seřazenými datumy
      */
     public static List<LocalDate> smallerFirst(LocalDate start, LocalDate end){
         return null;
@@ -92,7 +87,7 @@ public class DateTimeUtils {
     /**
      * Najděte a vypište první pondělí po Vánocích 24.12.2023
      *
-     * @return
+     * @return první pondělí po Vánocích (LocalDate)
      */
     public static LocalDate firstMondayAfterChristmas(){
         return null;
